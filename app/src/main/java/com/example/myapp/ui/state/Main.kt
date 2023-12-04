f3d76c6e113b7247e82cead7ccc639ab9e7f733b
AnTomS <thekeibee@yandex.ru>
2023-12-04 21:42:30 +0800

    Массивы и список основы

---
+++ app/src/main/java/com/example/myapp/ui/state/Main.kt
@@ -0,0 +1,19 @@
+package com.example.myapp.ui.state
+
+import kotlin.text.Typography.greater
+const val VALIDATE_PREMIUM : Int = 1500
+
+fun main(arrgs: Array<String>) {
+    println("делай осознанно")
+
+
+    val listOfIngredients2 = listOf("flour", "eggs", "milk")
+    listOfIngredients2.forEach {
+        println(it)
+    }
+
+var listOfIngredients = listOf(1, 98, 325, 3, 325, 56)
+
+
+}
+
